50cb17a17e557ba7cb7f5dd5b8fc922417adf3c2
jsusmachaca <falcorgd@gmail.com>
2024-10-29 13:37:08 -0500

    feat: adds error message

--- pkg/response/Json.go
+++ pkg/response/Json.go
@@ -5,11 +5,23 @@ import (
 	"net/http"
 )
 
-func WriteJson(w http.ResponseWriter, body any, statusCode int) {
+func JsonResponse(w http.ResponseWriter, body any, statusCode int) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 
 	if err := json.NewEncoder(w).Encode(body); err != nil {
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
+	}
+}
+
+func JsonErrorFromString(w http.ResponseWriter, errorMessage string, statusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	respone := map[string]string{
+		"error": errorMessage,
+	}
+	if err := json.NewEncoder(w).Encode(respone); err != nil {
+		http.Error(w, errorMessage, statusCode)
 	}
 }
